35689861863a5037dd3ce2643e2ab3e7057d10f0
ShaneWhitmore <shane.s.whitmore@gmail.com>
2023-09-23 18:45:43 +0100

    Set up key and map

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 function App() {
   return (
@@ -11,6 +12,15 @@ function App() {
       <Task title="Tidy" deadline="Today" />
     </div>
   );
+
+
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
 }
 
 export default App;
